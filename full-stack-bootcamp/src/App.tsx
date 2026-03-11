c7e0c806912f874ddcb86cbcad67b72cd8c8c176
IbrahimBahwal <ebraim43@gmail.com>
2026-03-11 18:32:04 +0000

    1st Front end commit

--- full-stack-bootcamp/src/App.tsx
+++ full-stack-bootcamp/src/App.tsx
@@ -9,6 +9,13 @@
 //   Step 7b → import TaskModal     → add handleToggleCompleted + wire <TaskModal />
 //   Step 9  → connect to the Flask backend (useEffect + handleSubmit)
 // ─────────────────────────────────────────────────────────────
+import backgroundPattern from "./assets/Background Vector.svg";
+import leftLantern from "./assets/left lanterns.png"
+import rightLantern from "./assets/Right lanterns.png"
+import Header from "./components/Header";
+import Input from "./components/Input";
+import ParticleBackground from "./components/ParticleBackground";
+import TaskCard from "./components/TaskCard";
 
 function App() {
   // TODO (Step 5b): Add state — openTaskId and tasks
@@ -16,8 +23,29 @@ function App() {
   // const [tasks, setTasks] = useState(taskData as Task[]);
 
   return (
-    <div>
+    <div className="min-h-screen bg-(--bg-dark) text-white relative overflow-hidden font-lexend flex flex-col items-center" style={{backgroundImage:`url(${backgroundPattern})`}}>
       {/* TODO (Step 2): Replace this div with the full background layout */}
+      <ParticleBackground/>
+      <img src={leftLantern} className="absolute top-0 left-0 w-32 md:w-70 opacity-80 z-2"/>
+      <img src={rightLantern} className="absolute top-0 right-0 w-32 md:w-70 opacity-80 z-2"/>
+
+      <div className="relative z-2 mx-auto w-[95vw] h-[90vh] border-[3px] border-(--text-cream)
+      flex flex-col items-center gap-10 py-12 px-10 shadow-2xl my-12">
+      
+      <Header/>
+      <Input/>
+      <TaskCard
+       title="Distribute Food"
+       description="Help distribute food packages to families in need during Ramadan."
+       date="Mar 15th 2026"
+       activeCrescents={4}
+       variant="wide"
+       volunteersNeeded={5}
+       onClick={() => console.log("Card clicked")}
+      />
+
+      </div>
+      
       {/* Hint: background SVG · left/right lanterns · gold border frame */}
     </div>
   );
